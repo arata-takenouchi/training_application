dfa211d5d8fb77824b003c1a4de7730240bc8e6f
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-09 22:44:42 +0900

    revision form

--- workerthread.py
+++ workerthread.py
@@ -8,6 +8,8 @@ from socket import socket
 from threading import Thread
 from typing import Optional, Tuple
 
+import urllib.parse
+
 
 class WorkerThread(Thread):
     # 実行ファイルのあるディレクトリ
@@ -37,92 +39,112 @@ class WorkerThread(Thread):
         """
 
         try:
+          # クライアントから送られてきたデータを取得する
+          request = self.client_socket.recv(4096)
 
-            # クライアントから送られてきたデータを取得する
-            request = self.client_socket.recv(4096)
+          # クライアントから送られてきたデータをファイルに書き出す
+          with open("server_recv.txt", "wb") as f:
+            f.write(request)
 
-            # クライアントから送られてきたデータをファイルに書き出す
-            with open("server_recv.txt", "wb") as f:
-                f.write(request)
+          # HTTPリクエストをパースする
+          method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-            # HTTPリクエストをパースする
-            method, path, http_version, request_header, request_body = self.parse_http_request(request)
+          response_body: bytes
+          content_type: Optional[str]
+          response_line: str
 
-            response_body: bytes
-            content_type: Optional[str]
-            response_line: str
-
-            if path == "/now":
-              html = f"""\
-                <html>
-                  <body>
-                    <h1>Now: {datetime.now()}</h1>
-                  </body>
-                </html>
-              """
-              response_body = textwrap.dedent(html).encode()
-
-              content_type = "text/html"
-
-              response_line = "HTTP/1.1 200 OK\r\n"
-
-            elif path == "/show_request":
-              html = f"""\
-                <html>
+          if path == "/now":
+            html = f"""\
+              <html>
                 <body>
-                  <h1>Request Line:</h1>
-                  <p>
-                    {method} {path} {http_version}
-                  </p>
-                  <h1>Headers:</h1>
-                  <pre>{pformat(request_header)}</pre>
-                  <h1>Body:</h1>
-                  <pre>{request_body.decode("utf-8", "ignore")}</pre>
+                  <h1>Now: {datetime.now()}</h1>
                 </body>
-                </html>
+              </html>
+            """
+            response_body = textwrap.dedent(html).encode()
+
+            content_type = "text/html; charset=UTF-8"
+
+            response_line = "HTTP/1.1 200 OK\r\n"
+
+          elif path == "/show_request":
+            html = f"""\
+              <html>
+              <body>
+                <h1>Request Line:</h1>
+                <p>
+                  {method} {path} {http_version}
+                </p>
+                <h1>Headers:</h1>
+                <pre>{pformat(request_header)}</pre>
+                <h1>Body:</h1>
+                <pre>{request_body.decode("utf-8", "ignore")}</pre>
+              </body>
+              </html>
+            """
+            response_body = textwrap.dedent(html).encode()
+
+            content_type = "text/html; charset=UTF-8"
+
+            response_line = "HTTP/1.1 200 OK\r\n"
+
+          elif path == "/parameters":
+            if method == "GET":
+              response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
+              content_type = "text/html; charset=UTF-8"
+              response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
+
+            elif method == "POST":
+              post_params = urllib.parse.parse_qs(request_body.decode())
+              html = f"""\
+                  <html>
+                  <body>
+                      <h1>Parameters:</h1>
+                      <pre>{pformat(post_params)}</pre>
+                  </body>
+                  </html>
               """
               response_body = textwrap.dedent(html).encode()
 
-              content_type = "text/html"
+              content_type = "text/html; charset=UTF-8"
 
               response_line = "HTTP/1.1 200 OK\r\n"
+          else:
+            try:
+              # ファイルからレスポンスボディを生成
+              response_body = self.get_static_file_content(path)
+              content_type = None
 
-            else:
-              try:
-                # ファイルからレスポンスボディを生成
-                response_body = self.get_static_file_content(path)
-                content_type = None
+              # レスポンスラインを生成
+              response_line = "HTTP/1.1 200 OK\r\n"
 
-                # レスポンスラインを生成
-                response_line = "HTTP/1.1 200 OK\r\n"
+            except OSError:
+              traceback.print_exc()
 
-              except OSError:
-                traceback.print_exc
+              # ファイルが見つからなかった場合は404を返す
+              response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+              content_type = "text/html; charset=UTF-8"
+              response_line = "HTTP/1.1 404 Not Found\r\n"
 
-                # ファイルが見つからなかった場合は404を返す
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                content_type = "text/html"
-                response_line = "HTTP/1.1 404 Not Found\r\n"
+          # レスポンスヘッダーを生成
+          response_header = self.build_response_header(path, response_body, content_type)
 
-            # レスポンスヘッダーを生成
-            response_header = self.build_response_header(path, response_body, content_type)
+          # レスポンス全体を生成する
+          response = (response_line + response_header + "\r\n").encode() + response_body
 
-            # レスポンス全体を生成する
-            response = (response_line + response_header + "\r\n").encode() + response_body
-
-            # クライアントへレスポンスを送信する
-            self.client_socket.send(response)
+          # クライアントへレスポンスを送信する
+          self.client_socket.send(response)
 
         except Exception:
-            # リクエストの処理中に例外が発生した場合はコンソールにエラーログを出力し、
-            # 処理を続行する
-            print("=== Worker: リクエストの処理中にエラーが発生しました ===")
-            traceback.print_exc()
+          # リクエストの処理中に例外が発生した場合はコンソールにエラーログを出力し、
+          # 処理を続行する
+          print("=== Worker: リクエストの処理中にエラーが発生しました ===")
+          traceback.print_exc()
 
         finally:
-            # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
-            print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
-            self.client_socket.close()
+          # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
+          print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
+          self.client_socket.close()
 
     def parse_http_request(self, request: bytes) -> Tuple[str, str, str, dict, bytes]:
         """
@@ -164,7 +186,7 @@ class WorkerThread(Thread):
         static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
 
         with open(static_file_path, "rb") as f:
-            return f.read()
+          return f.read()
 
     def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
         """
@@ -172,9 +194,9 @@ class WorkerThread(Thread):
         """
         if content_type is None:
           if "." in path:
-              ext = path.rsplit(".", maxsplit=1)[-1]
+            ext = path.rsplit(".", maxsplit=1)[-1]
           else:
-              ext = ""
+            ext = ""
           # 拡張子からMIME Typeを取得
           # 知らない対応していない拡張子の場合はoctet-streamとする
           content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
@@ -187,3 +209,4 @@ class WorkerThread(Thread):
         response_header += f"Content-Type: {content_type}\r\n"
 
         return response_header
+
