fa1e04f2b3a56e76c3a486186287f652374d94ae
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-09 18:48:20 +0900

    add multi thread

--- webserver.py
+++ webserver.py
@@ -1,24 +1,25 @@
-import os
 import socket
-from datetime import datetime
-import traceback
-from typing import Tuple
+
+from workerthread import WorkerThread
 
 class WebServer:
-  """
-  Webサーバーを表すクラス
-  """
 
-  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-  STATIC_ROOT = os.path.join(BASE_DIR, "static")
+  def serve(self):
+    print("=== Server: サーバーを起動します ===")
 
-  MIME_TYPES = {
-    "html": "text/html",
-    "css": "text/css",
-    "png": "image/png",
-    "jpg": "image/jpg",
-    "gif": "image/gif",
-  }
+    try:
+      server_socket = self.create_server_socket()
+
+      while True:
+        print("=== Server: クライアントからの接続を待ちます ===")
+        (client_socket, address) = server_socket.accept()
+        print(f"=== Server: クライアントとの接続が完了しました remote_address: {address} ===")
+
+        thread = WorkerThread(client_socket, address)
+        thread.start()
+
+    finally:
+      print("=== Server: サーバーを停止します。 ===")
 
   def create_server_socket(self) -> socket:
     server_socket = socket.socket()
@@ -28,84 +29,6 @@ class WebServer:
     server_socket.listen(10)
     return server_socket
 
-  def handle_client(self, client_socket: socket) -> None:
-    request = client_socket.recv(4096)
-
-    with open("server_recv", "wb") as f:
-      f.write(request)
-
-    method, path, http_version, request_header, request_body = self.parse_http_request(request)
-
-    try:
-      response_body = self.get_static_file_content(path)
-
-      response_line = "HTTP/1.1 200 OK\r\n"
-
-    except OSError:
-      response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-      response_line = "HTTP/1.1 404 Not Found\r\n"
-
-    response_header = self.build_response_header(path, response_body)
-
-    response = (response_line + response_header + "\r\n").encode() + response_body
-
-    client_socket.send(response)
-
-  def parse_http_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
-    request_line, remain = request.split(b"\r\n", maxsplit=1)
-    request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
-
-    method, path, http_version = request_line.decode().split(" ")
-
-    return method, path, http_version, request_header, request_body
-
-  def get_static_file_content(self, path: str) -> bytes:
-    relative_path = path.lstrip("/")
-    static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
-
-    with open(static_file_path, "rb") as f:
-      return f.read()
-
-  def build_response_header(self, path: str, response_body: bytes) -> str:
-    if "." in path:
-      ext = path.rsplit(".", maxsplit=1)[-1]
-    else:
-      ext = ""
-    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
-
-    response_header = ""
-    response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-    response_header += "Host: HenaServer/0.1\r\n"
-    response_header += f"Content-Length: {len(response_body)}\r\n"
-    response_header += "Connection: Close\r\n"
-    response_header += f"Content-type: {content_type}\r\n"
-
-    return response_header
-
-  def serve(self):
-    print("=== start server ===")
-
-    try:
-      server_socket = self.create_server_socket()
-
-      while True:
-        print("=== wait client connection ===")
-        (client_socket, address) = server_socket.accept()
-        print(f"=== complete client connection remote_address: {address} ===")
-
-        try:
-          self.handler_client(client_socket)
-
-        except Exception:
-          print("=== error in process ===")
-          traceback.print_exc()
-
-        finally:
-          client_socket.close()
-
-    finally:
-      print("=== stop server ===")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   server = WebServer()
   server.serve()
