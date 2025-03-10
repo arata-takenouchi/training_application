d24a5889d098ef89bb3cf6a54d0382f081e86d2c
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-10 23:49:08 +0900

    refactor

--- workerthread.py
+++ workerthread.py
@@ -1,14 +1,9 @@
 import os
-from pprint import pformat
 import re
-import textwrap
 import traceback
-from datetime import datetime
 from socket import socket
 from threading import Thread
-from typing import Optional, Tuple
-
-import urllib.parse
+import views
 
 
 class WorkerThread(Thread):
@@ -54,61 +49,16 @@ class WorkerThread(Thread):
           response_line: str
 
           if path == "/now":
-            html = f"""\
-              <html>
-                <body>
-                  <h1>Now: {datetime.now()}</h1>
-                </body>
-              </html>
-            """
-            response_body = textwrap.dedent(html).encode()
-
-            content_type = "text/html; charset=UTF-8"
-
-            response_line = "HTTP/1.1 200 OK\r\n"
+            response_body, content_type, response_line = view.now()
 
           elif path == "/show_request":
-            html = f"""\
-              <html>
-              <body>
-                <h1>Request Line:</h1>
-                <p>
-                  {method} {path} {http_version}
-                </p>
-                <h1>Headers:</h1>
-                <pre>{pformat(request_header)}</pre>
-                <h1>Body:</h1>
-                <pre>{request_body.decode("utf-8", "ignore")}</pre>
-              </body>
-              </html>
-            """
-            response_body = textwrap.dedent(html).encode()
-
-            content_type = "text/html; charset=UTF-8"
-
-            response_line = "HTTP/1.1 200 OK\r\n"
+            response_body, content_type, response_line = view.show_request(
+              method, path, http_version, request_header, request_body
+            )
 
           elif path == "/parameters":
-            if method == "GET":
-              response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-              content_type = "text/html; charset=UTF-8"
-              response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
+            response_body, content_type, response_line = view.parameters(method, request_body)
 
-            elif method == "POST":
-              post_params = urllib.parse.parse_qs(request_body.decode())
-              html = f"""\
-                  <html>
-                  <body>
-                      <h1>Parameters:</h1>
-                      <pre>{pformat(post_params)}</pre>
-                  </body>
-                  </html>
-              """
-              response_body = textwrap.dedent(html).encode()
-
-              content_type = "text/html; charset=UTF-8"
-
-              response_line = "HTTP/1.1 200 OK\r\n"
           else:
             try:
               # ファイルからレスポンスボディを生成
