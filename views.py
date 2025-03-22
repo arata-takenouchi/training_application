4073fbb65116bc01e34031badb992e7c72ada5dd
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-22 20:02:41 +0900

    revision views

--- views.py
+++ views.py
@@ -1,4 +1,3 @@
-import textwrap
 import urllib.parse
 from datetime import datetime
 from pprint import pformat
@@ -14,58 +13,23 @@ def now(request: HTTPRequest) -> HTTPResponse:
   return HTTPResponse(body=body)
 
 def show_request(request: HTTPRequest) -> HTTPResponse:
-  html = f"""\
-    <html>
-    <body>
-      <h1>Request Line:</h1>
-      <p>
-        {method} {path} {http_version}
-      </p>
-      <h1>Headers:</h1>
-      <pre>{pformat(request_header)}</pre>
-      <h1>Body:</h1>
-      <pre>{request_body.decode("utf-8", "ignore")}</pre>
-    </body>
-    </html>
-  """
-  body = textwrap.dedent(html).encode()
+  context = {"request": request, "headers": pformat(request.headers), "body": request.body.decode("utf-8", "ignore")}
+  body = render("show_request.html", context)
 
   return HTTPResponse(body=body)
 
 def parameters(request: HTTPRequest) -> HTTPResponse:
   if method == "GET":
     body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-    content_type = "text/html; charset=UTF-8"
-    status_code = 405
+    return HTTPResponse(body=body)
 
   elif method == "POST":
-    post_params = urllib.parse.parse_qs(request_body.decode())
-    html = f"""\
-        <html>
-        <body>
-            <h1>Parameters:</h1>
-            <pre>{pformat(post_params)}</pre>
-        </body>
-        </html>
-    """
-    response_body = textwrap.dedent(html).encode()
-    content_type = "text/html; charset=UTF-8"
-    status_code = 200
-
-  return HTTPResponse(body=response_body, content_type=content_type, status_code=status_code)
+    context = {"params": urllib.parse.parse_qs(request.body.decode())}
+    body = render("parameters.html", context)
+    return HTTPResponse(body=body)
 
 def user_profile(request: HTTPRequest) -> HTTPResponse:
-  user_id = request.params["user_id"]
-  html = f"""\
-      <html>
-      <body>
-          <h1>プロフール</h1>
-          <pre>ID: {user_id}</pre>
-      </body>
-      </html>
-  """
-  body = textwrap.dedent(html).encode()
-  content_type = "text/html; charset=UTF-8"
-  status_code = 200
+  context = {"user_id": request.params["user_id"]}
+  body = render("user_profile.html", context)
 
-  return HTTPResponse(body=body, content_type=content_type, status_code=status_code)
+  return HTTPResponse(body=body)
