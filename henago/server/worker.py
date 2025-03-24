e5c9201843b2976d57ebf17def6688589ec644ca
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-24 23:52:11 +0900

    revision worker

--- henago/server/worker.py
+++ henago/server/worker.py
@@ -1,13 +1,14 @@
-import os
 import re
 import traceback
+from datetime import datetime
 from socket import socket
 from threading import Thread
 from typing import Tuple
 
-from henago.http.request import HTTPRequest
-from henago.http.response import HTTPResponse
-from henago.urls.resolver import URLResolver
+from henango.http.request import HTTPRequest
+from henango.http.response import HTTPResponse
+from henango.urls.resolver import URLResolver
+
 
 class Worker(Thread):
     # 拡張子とMIME Typeの対応
@@ -19,14 +20,15 @@ class Worker(Thread):
         "gif": "image/gif",
     }
 
+    # ステータスコードとステータスラインの対応
     STATUS_LINES = {
-      200: "200 OK",
-      302: "302 Found",
-      404: "404 Not Found",
-      405: "405 Method Not Allowed",
+        200: "200 OK",
+        302: "302 Found",
+        404: "404 Not Found",
+        405: "405 Method Not Allowed",
     }
 
-    def __init__(self, client_socket: socket, address: tuple[str, int]):
+    def __init__(self, client_socket: socket, address: Tuple[str, int]):
         super().__init__()
 
         self.client_socket = client_socket
@@ -39,56 +41,54 @@ class Worker(Thread):
         """
 
         try:
-          # クライアントから送られてきたデータを取得する
-          request_bytes = self.client_socket.recv(4096)
 
-          # クライアントから送られてきたデータをファイルに書き出す
-          with open("server_recv.txt", "wb") as f:
-            f.write(request_bytes)
+            # クライアントから送られてきたデータを取得する
+            request_bytes = self.client_socket.recv(4096)
 
-          # HTTPリクエストをパースする
-          request = self.parse_http_request(request_bytes)
+            # クライアントから送られてきたデータをファイルに書き出す
+            with open("server_recv.txt", "wb") as f:
+                f.write(request_bytes)
 
-          view = URLResolver().resolve(request)
+            # HTTPリクエストをパースする
+            request = self.parse_http_request(request_bytes)
 
-          response = view(request)
+            # URL解決を行う
+            view = URLResolver().resolve(request)
 
-          if isinstance(response.body, str):
-            response.body = response.body.encode()
+            # レスポンスを生成する
+            response = view(request)
 
-          response_line = self.build_response_line(response)
+            # レスポンスボディを変換
+            if isinstance(response.body, str):
+                response.body = response.body.encode()
 
-          # レスポンスヘッダーを生成
-          response_header = self.build_response_header(response, request)
+            # レスポンスラインを生成
+            response_line = self.build_response_line(response)
 
-          # レスポンス全体を生成する
-          response_bytes = (response_line + response_header + "\r\n").encode() + response.body
+            # レスポンスヘッダーを生成
+            response_header = self.build_response_header(response, request)
 
-          # クライアントへレスポンスを送信する
-          self.client_socket.send(response_bytes)
+            # レスポンス全体を生成する
+            response_bytes = (response_line + response_header + "\r\n").encode() + response.body
+
+            # クライアントへレスポンスを送信する
+            self.client_socket.send(response_bytes)
 
         except Exception:
-          # リクエストの処理中に例外が発生した場合はコンソールにエラーログを出力し、
-          # 処理を続行する
-          print("=== Worker: リクエストの処理中にエラーが発生しました ===")
-          traceback.print_exc()
+            # リクエストの処理中に例外が発生した場合はコンソールにエラーログを出力し、
+            # 処理を続行する
+            print("=== Worker: リクエストの処理中にエラーが発生しました ===")
+            traceback.print_exc()
 
         finally:
-          # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
-          print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
-          self.client_socket.close()
+            # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
+            print(f"=== Worker: クライアントとの通信を終了します remote_address: {self.client_address} ===")
+            self.client_socket.close()
 
-    def parse_http_request(self, request: bytes) -> tuple[str, str, str, dict, bytes]:
+    def parse_http_request(self, request: bytes) -> HTTPRequest:
         """
-        HTTPリクエストを
-        1. method: str
-        2. path: str
-        3. http_version: str
-        4. request_header: bytes
-        5. request_body: bytes
-        に分割/変換する
+        生のHTTPリクエストを、HTTPRequestクラスへ変換する
         """
-
         # リクエスト全体を
         # - リクエストライン(1行目)
         # - リクエストヘッダー(2行目〜空行)
@@ -100,45 +100,48 @@ class Worker(Thread):
         # リクエストラインを文字列に変換してパースする
         method, path, http_version = request_line.decode().split(" ")
 
+        # リクエストヘッダーを辞書にパースする
         headers = {}
         for header_row in request_header.decode().split("\r\n"):
-          key, value = re.split(r": *", header_row, maxsplit=1)
-          headers[key] = value
+            key, value = re.split(r": *", header_row, maxsplit=1)
+            headers[key] = value
 
-        return method, path, http_version, headers, request_body
-        return HTTPRequest(method=method, path=path, http_version=http_version, headers=headers, request_body=request_body)
+        cookies = {}
+        if "Cookie" in headers:
+            # str から list へ変換 (ex) "name1=value1; name2=value2" => ["name1=value1", "name2=value2"]
+            cookie_strings = headers["Cookie"].split("; ")
+            # list から dict へ変換 (ex) ["name1=value1", "name2=value2"] => {"name1": "value1", "name2": "value2"}
+            for cookie_string in cookie_strings:
+                name, value = cookie_string.split("=", maxsplit=1)
+                cookies[name] = value
 
-    def get_static_file_content(self, path: str) -> bytes:
-        """
-        リクエストpathから、staticファイルの内容を取得する
-        """
-        default_static_root = os.path.join(os.path.dirname(__file__), "../../static")
-        static_root = getattr(settings, "STATIC_ROOT", default_static_root)
-
-        # pathの先頭の/を削除し、相対パスにしておく
-        relative_path = path.lstrip("/")
-        # ファイルのpathを取得
-        static_file_path = os.path.join(static_root, relative_path)
-
-        with open(static_file_path, "rb") as f:
-          return f.read()
+        return HTTPRequest(
+            method=method, path=path, http_version=http_version, headers=headers, cookies=cookies, body=request_body
+        )
 
     def build_response_line(self, response: HTTPResponse) -> str:
-      status_line = self.STATUS_LINES[response.status_code]
-      return f"HTTP/1.1 {status_line}"
+        """
+        レスポンスラインを構築する
+        """
+        status_line = self.STATUS_LINES[response.status_code]
+        return f"HTTP/1.1 {status_line}"
 
     def build_response_header(self, response: HTTPResponse, request: HTTPRequest) -> str:
         """
         レスポンスヘッダーを構築する
         """
+
+        # Content-Typeが指定されていない場合はpathから特定する
         if response.content_type is None:
-          if "." in request.path:
-            ext = request.path.rsplit(".", maxsplit=1)[-1]
-            # 拡張子からMIME Typeを取得
-            # 知らない対応していない拡張子の場合はoctet-streamとする
-            response.content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
-          else:
-            response.content_type = "text/html; charset=UTF-8"
+            # pathから拡張子を取得
+            if "." in request.path:
+                ext = request.path.rsplit(".", maxsplit=1)[-1]
+                # 拡張子からMIME Typeを取得
+                # 知らない対応していない拡張子の場合はoctet-streamとする
+                response.content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+            else:
+                # pathに拡張子がない場合はhtml扱いとする
+                response.content_type = "text/html; charset=UTF-8"
 
         response_header = ""
         response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
@@ -147,7 +150,10 @@ class Worker(Thread):
         response_header += "Connection: Close\r\n"
         response_header += f"Content-Type: {response.content_type}\r\n"
 
+        for cookie_name, cookie_value in response.cookies.items():
+            response_header += f"Set-Cookie: {cookie_name}={cookie_value}\r\n"
+
         for header_name, header_value in response.headers.items():
-          response_header += f"{header_name}: {header_value}\r\n"
+            response_header += f"{header_name}: {header_value}\r\n"
 
         return response_header
